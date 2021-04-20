fa3e6261b0d4ecd5a431f7956070caa983c9a974
Ivan <89821080113@mail.ru>
2021-04-21 01:11:56 +0300

    creating the initial version of the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import s from './Header.module.scss';
+import logo from './../../img/logo.svg';
+import ava from './../../img/AvatarCat.jpg';
 
 const Header = () => (
     <div className={s.header}>
-            Header
+        <div className={s.leftHeaderPart}>
+            <div className={s.logoWrapper}>
+                <div className={s.logo}><img src={logo} alt=""/></div>
+            </div>
+            <div className={s.textWrapper}>
+                <div className={s.text}>my react-redux spa</div>
+            </div>
+        </div>
+        <div className={s.rightHeaderPart}>
+            <div className={s.name}>Ivan</div>
+            <div className={s.photoWrapper}><img src={ava} alt=""/></div>
+            <div className={s.burger}>X</div>
+        </div>
+
     </div>
 )
-
 
 
 export default Header;
