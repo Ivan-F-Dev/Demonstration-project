7d9590d82232075aef2de3b6fde904ce0a28aaa6
Roman Shchukin <Tankeast74@yandex.ru>
2021-05-05 20:14:10 +0500

    Password field border radius fix

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,13 +1,18 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import s from './LoginPage.module.scss'
-import {Button, makeStyles, TextField} from "@material-ui/core";
+import { Button, makeStyles, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: "flex",
         justifyContent: "center",
         borderTopLeftRadius: 0,
-        borderTopRightRadius: 0
+        borderTopRightRadius: 0,
+
+        '& .MuiFilledInput-root': {
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0
+        }
     },
 }));
 
@@ -23,11 +28,11 @@ let LoginPage = (props) => {
         <div>
             <div>
                 <TextField onChange={(event) => setLoginInput(event.target.value)} value={loginInput} fullWidth={true}
-                           color="primary" label="Write a new post" variant="filled"/>
-                <TextField onChange={(event) => setPasswordInput(event.target.value)}  value={passwordInput} fullWidth={true}
-                           className={classes.root} color="primary" label="Write a new post" variant="filled"/>
+                    color="primary" label="Write a new post" variant="filled" />
+                <TextField onChange={(event) => setPasswordInput(event.target.value)} value={passwordInput} fullWidth={true}
+                    className={classes.root} color="primary" label="Write a new post" variant="filled" />
                 <Button variant="contained" className={classes.root} color="primary"
-                        fullWidth={true}>Login</Button>
+                    fullWidth={true}>Login</Button>
             </div>
         </div>
     )
